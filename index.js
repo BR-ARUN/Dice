8ef4b8d8881d388faa73d884df8983e24a4d8c96
BR-ARUN <86709852+BR-ARUN@users.noreply.github.com>
2021-10-07 20:44:33 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ randomnunber2=Math.floor(Math.random()*6+1);
 randomnunber1=Math.floor(Math.random()*6+1);
 
 
-document.querySelector("img.img1").setAttribute("src","images/dice"+randomnunber1+".png");
-document.querySelector("img.img2").setAttribute("src","images/dice"+randomnunber2+".png");
+document.querySelector("img.img1").setAttribute("src","/images/dice"+randomnunber1+".png");
+document.querySelector("img.img2").setAttribute("src","/images/dice"+randomnunber2+".png");
 if(randomnunber1>randomnunber2)
 {
 document.querySelector("h1").textContent="Player 1 wins!";
