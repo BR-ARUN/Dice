6b7f1dca5279efa42b49d3da858d14d07ff4e178
BR-ARUN <86709852+BR-ARUN@users.noreply.github.com>
2021-10-07 20:31:22 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var randomnunber1,randomnunber2;
+
+randomnunber2=Math.floor(Math.random()*6+1);
+randomnunber1=Math.floor(Math.random()*6+1);
+
+
+document.querySelector("img.img1").setAttribute("src","/Dicee Challenge - Starting Files/images/dice"+randomnunber1+".png");
+document.querySelector("img.img2").setAttribute("src","/Dicee Challenge - Starting Files/images/dice"+randomnunber2+".png");
+if(randomnunber1>randomnunber2)
+{
+document.querySelector("h1").textContent="Player 1 wins!";
+}
+else
+if(randomnunber2>randomnunber1){
+    document.querySelector("h1").textContent="Player 2 wins!";
+}
+else{
+    document.querySelector("h1").textContent="Draw!";
+}
